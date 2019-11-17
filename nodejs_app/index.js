1a5d78c40b834ddf9a3e90528ed1803f7d8bc5d8
Abhishek <devopsabi@gmail.com>
2019-11-17 13:03:12 +0000

    updated color index.js

--- nodejs_app/index.js
+++ nodejs_app/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World! <h1 style="color:blue;">Demo App Version 03<h1>'))
+var os = require("os");
+var hostname = os.hostname();
+
+app.get('/', (req, res) => res.send('Hello World! <h1 style="color:blue;">Demo App Version 03<h1> <br>hostname'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
