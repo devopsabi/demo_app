1fe8f93836e62b0b20e630be6f739a890035cc49
devopsabi <devopsabi@gmail.com>
2019-11-20 10:39:28 +0000

    Updated Jenkins file

--- health-check.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-status=$(ping -c2 192.168.33.11 | grep "packet loss" | cut -d, -f3)
-echo "docker swarm manager has $status"
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ssh -q jenkins@192.168.33.11 'docker node ls'
