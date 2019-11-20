df6bbdbbae61a83af3a583c3e95b116cc2cadf41
devopsabi <57540610+devopsabi@users.noreply.github.com>
2019-11-20 16:27:37 +0100

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-status=$(ping -c2 192.168.33.11 | grep "packet loss" | cut -d, -f3)
+status=$(ping -c2 192.168.33.10 | grep "packet loss" | cut -d, -f3)
 echo "docker swarm manager has $status"
